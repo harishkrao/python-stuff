b36c3fb10badf0441a040cc062bd39396047b4bd
Harish Kesava Rao <harishkesavarao@linux-78fg.attlocal.net>
2018-04-07 20:46:09 -0500

    base class, a sample function

---
+++ run_script.py
@@ -0,0 +1,5 @@
+import warmup_1
+
+a = warmup_1.warmup_exercises()
+
+a.sleep_in('a')
---
+++ warmup_1.py
@@ -0,0 +1,10 @@
+class warmup_exercises():
+    def sleep_in(weekday, vacation):
+        return weekday, vacation
+
+
+    def main():
+        sleep_in(weekday, vacation)
+
+    if __name__ == 'main':
+        main()
